b6b8a4063fe0db9dd49830f9e0c92d3ce56f2178
Andreas Bartilson <andreas.bartilson@gmail.com>
2023-01-24 22:19:08 +0100

    Add API skeleton. Update gitignore.

---
+++ StudyPlannerAPI/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace StudyPlannerAPI
+{
+    public class Constants
+    {
+        public static readonly string API_NAME = "studyplanner";
+
+        public const string ROUTE_MASTER_CHECK = "studyplanner/mastercheck";
+        public const string ROUTE_COURSE_DATA = "studyplanner/coursedata";
+    }
+}
---
+++ StudyPlannerAPI/Controllers/CourseDataController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudyPlannerAPI.Controllers
+{
+    [Route(Constants.ROUTE_COURSE_DATA)]
+    [ApiController]
+    public class CourseDataController : ControllerBase
+    {
+        private ILogger<CourseDataController> logger;
+
+        public CourseDataController(ILogger<CourseDataController> logger)
+        {
+            this.logger = logger;
+        }
+
+
+        // TODO: endpoint, get course catalog (page parameter?)
+
+        // TODO: endpoint, filter course catalog
+    }
+}
