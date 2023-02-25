5440a5ee54c995f18f4dc78df5b58ae78d6bc49c
Pontus Salenbo <pontuss98@hotmail.com>
2023-02-25 22:20:27 +0100

    refactor(hooks): fix typing errors

--- StudyPlannerUI/src/hooks/usePagination.tsx
+++ StudyPlannerUI/src/hooks/usePagination.tsx
@@ -2,7 +2,7 @@ import { useMemo } from 'react';
 
 export const DOTS = '...';
 
-const range = (start: number, end: number) => {
+const range = (start: number, end: number): number[] => {
 	const length = end - start + 1;
 	return Array.from({ length }, (_, idx) => idx + start);
 };
@@ -19,7 +19,7 @@ export const usePagination = ({
 	pageSize,
 	siblingCount = 1,
 	currentPage
-}: IUsePaginationProps) => {
+}: IUsePaginationProps): (number | string)[] => {
 	const paginationRange = useMemo(() => {
 		const totalPageCount = Math.ceil(totalCount / pageSize);
 
@@ -39,10 +39,7 @@ export const usePagination = ({
           Calculate left and right sibling index and make sure they are within range 1 and totalPageCount
       */
 		const leftSiblingIndex = Math.max(currentPage - siblingCount, 1);
-		const rightSiblingIndex = Math.min(
-			currentPage + siblingCount,
-			totalPageCount
-		);
+		const rightSiblingIndex = Math.min(currentPage + siblingCount, totalPageCount);
 
 		/*
         We do not show dots just when there is just one page number to be inserted between the extremes of sibling and the page limits i.e 1 and totalPageCount. Hence we are using leftSiblingIndex > 2 and rightSiblingIndex < totalPageCount - 2
@@ -68,10 +65,7 @@ export const usePagination = ({
       */
 		if (shouldShowLeftDots && !shouldShowRightDots) {
 			const rightItemCount = 3 + 2 * siblingCount;
-			const rightRange = range(
-				totalPageCount - rightItemCount + 1,
-				totalPageCount
-			);
+			const rightRange = range(totalPageCount - rightItemCount + 1, totalPageCount);
 			return [firstPageIndex, DOTS, ...rightRange];
 		}
 
