5527987a0033746f5792a137a434f6ba39f155fc
Pontus Salenbo <pontuss98@hotmail.com>
2023-02-25 22:12:00 +0100

    feat(grid): add flexible Grid components

---
+++ StudyPlannerUI/src/utils/breakpoints.ts
@@ -0,0 +1,20 @@
+export const sizes = {
+	mobileS: '320px',
+	mobileL: '600px',
+	tablet: '900px',
+	laptop: '1200px',
+	desktop: '1536px'
+};
+
+export const device = {
+	/** xs-breakpoint */
+	mobileS: `(min-width: ${sizes.mobileS})`,
+	/** sm-breakpoint */
+	mobileL: `(min-width: ${sizes.mobileL})`,
+	/** md-breakpoint */
+	tablet: `(min-width: ${sizes.tablet})`,
+	/** lg-breakpoint */
+	laptop: `(min-width: ${sizes.laptop})`,
+	/** xl-breakpoint */
+	desktop: `(min-width: ${sizes.desktop})`
+};
