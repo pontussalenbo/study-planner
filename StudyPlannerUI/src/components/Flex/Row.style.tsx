7b218131f51a87872e1ed4d5824aa4221078b836
Pontus Salenbo <pontuss98@hotmail.com>
2023-02-26 13:19:59 +0100

    style: prettier bump. Replace tab indent with spaces

--- StudyPlannerUI/src/components/Flex/Row.style.tsx
+++ StudyPlannerUI/src/components/Flex/Row.style.tsx
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 
 const Row = styled.div`
-	display: flex;
-	flex-wrap: wrap;
-	margin-right: -15px;
-	margin-left: -15px;
+    display: flex;
+    flex-wrap: wrap;
+    margin-right: -15px;
+    margin-left: -15px;
 `;
 
 export default Row;
--- StudyPlannerUI/src/components/oldTable.tsx
+++ StudyPlannerUI/src/components/oldTable.tsx
@@ -2,35 +2,35 @@
 import React, { useEffect } from 'react';
 
 function Table(props: any) {
-	const headers = ['Course Code', 'Credits', 'Level', 'Name'];
-	const contents = ['courseCode', 'credits', 'cycle', 'name_en'];
+    const headers = ['Course Code', 'Credits', 'Level', 'Name'];
+    const contents = ['courseCode', 'credits', 'cycle', 'name_en'];
 
-	useEffect(() => {
-		const { courses } = props;
-		console.log(courses);
-	}, [props]);
-	return (
-		<div className='container'>
-			<table className='table b-table w-100 table-striped table-hover table-sm text-left'>
-				<thead className='thead-dark'>
-					<tr>
-						{headers.map(header => (
-							<th className='p-2'>{header}</th>
-						))}
-					</tr>
-				</thead>
-				<tbody>
-					{props.courses.map((course: never) => (
-						<tr>
-							{contents.map(content => (
-								<td>{course[content]}</td>
-							))}
-						</tr>
-					))}
-				</tbody>
-			</table>
-		</div>
-	);
+    useEffect(() => {
+        const { courses } = props;
+        console.log(courses);
+    }, [props]);
+    return (
+        <div className='container'>
+            <table className='table b-table w-100 table-striped table-hover table-sm text-left'>
+                <thead className='thead-dark'>
+                    <tr>
+                        {headers.map(header => (
+                            <th className='p-2'>{header}</th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.courses.map((course: never) => (
+                        <tr>
+                            {contents.map(content => (
+                                <td>{course[content]}</td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default Table;
