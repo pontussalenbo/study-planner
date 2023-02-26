90ad773c8307a25fa703f051d68b0d14f953eb21
Pontus Salenbo <pontuss98@hotmail.com>
2023-02-26 21:59:39 +0100

    feat(views): add MainPage to React application

---
+++ StudyPlannerUI/src/views/MainPage/style.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+`;
+
+export const Wrapper = styled.div`
+    width: 90%;
+`;
