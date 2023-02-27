8334ca85796aa72d564bfd7e7ff7984f023dfb82
Pontus Salenbo <pontuss98@hotmail.com>
2023-02-27 13:51:45 +0100

    build(vite): add bundle analyzer

--- StudyPlannerUI/vite.config.ts
+++ StudyPlannerUI/vite.config.ts
@@ -4,59 +4,66 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig(({ mode }) => ({
-	build: {
-		outDir: 'build'
-	},
-	test: {
-		css: false,
-		include: ['src/**/__tests__/*'],
-		globals: true,
-		environment: 'jsdom',
-		setupFiles: 'src/setupTests.ts',
-		clearMocks: true,
-		coverage: {
-			provider: 'istanbul',
-			enabled: true,
-			'100': true,
-			reporter: ['text', 'lcov'],
-			reportsDirectory: 'coverage'
-		}
-	},
-	plugins: [
-		tsconfigPaths(),
-		react(),
-		...(mode === 'test'
-			? []
-			: [
-					eslintPlugin(),
-					VitePWA({
-						registerType: 'autoUpdate',
-						includeAssets: [
-							'favicon.png',
-							'robots.txt',
-							'apple-touch-icon.png',
-							'icons/*.svg',
-							'fonts/*.woff2'
-						],
-						manifest: {
-							theme_color: '#BD34FE',
-							icons: [
-								{
-									src: '/android-chrome-192x192.png',
-									sizes: '192x192',
-									type: 'image/png',
-									purpose: 'any maskable'
-								},
-								{
-									src: '/android-chrome-512x512.png',
-									sizes: '512x512',
-									type: 'image/png'
-								}
-							]
-						}
-					})
-			  ])
-	]
+    build: {
+        outDir: 'build',
+        rollupOptions: {
+            manualChunks: {
+                recharts: ['recharts']
+            }
+        }
+    },
+    test: {
+        css: false,
+        include: ['src/**/__tests__/*'],
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: 'src/setupTests.ts',
+        clearMocks: true,
+        coverage: {
+            provider: 'istanbul',
+            enabled: true,
+            '100': true,
+            reporter: ['text', 'lcov'],
+            reportsDirectory: 'coverage'
+        }
+    },
+    plugins: [
+        tsconfigPaths(),
+        react(),
+        ...(mode === 'test'
+            ? []
+            : [
+                  eslintPlugin(),
+                  VitePWA({
+                      registerType: 'autoUpdate',
+                      includeAssets: [
+                          'favicon.png',
+                          'robots.txt',
+                          'apple-touch-icon.png',
+                          'icons/*.svg',
+                          'fonts/*.woff2'
+                      ],
+                      manifest: {
+                          theme_color: '#BD34FE',
+                          icons: [
+                              {
+                                  src: '/android-chrome-192x192.png',
+                                  sizes: '192x192',
+                                  type: 'image/png',
+                                  purpose: 'any maskable'
+                              },
+                              {
+                                  src: '/android-chrome-512x512.png',
+                                  sizes: '512x512',
+                                  type: 'image/png'
+                              }
+                          ]
+                      }
+                  })
+              ]),
+        visualizer()
+    ]
 }));
