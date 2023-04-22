57aeac60eae6ba3c624f038121f2ccf650079510
Pontus Salenbo <pontuss98@hotmail.com>
2023-04-22 17:33:48 +0200

    chore(typings): update and improve typings

--- StudyPlannerUI/src/interfaces/Event.d.ts
+++ StudyPlannerUI/src/interfaces/Event.d.ts
@@ -1,3 +1,7 @@
+/**
+ * This file is used to define event types for the application.
+ * This is done to avoid extensive use of the 'any' type and long type definitions.
+ */
 import type React from 'react';
 
 export type Event<T extends HTMLElement> = React.ChangeEvent<T>;
