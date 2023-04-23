1eeff264ba052d8df62747e60ce556a880776e75
Andreas Bartilson <andreas.bartilson@gmail.com>
2023-04-23 23:58:58 +0200

    Initial structure for linksharing.

---
+++ StudyPlannerAPI/Controllers/LinkShareController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudyPlannerAPI.Controllers.Params;
+using StudyPlannerAPI.Model;
+
+namespace StudyPlannerAPI.Controllers;
+
+[Route(Constants.ROUTE_LINK_SHARE)]
+[ApiController]
+public class LinkShareController : ControllerBase
+{
+    private readonly ILinkShareManager linkShareManager;
+    private readonly ILogger<LinkShareController> logger;
+
+    public LinkShareController(ILinkShareManager linkShareManager, ILogger<LinkShareController> logger)
+    {
+        this.linkShareManager = linkShareManager;
+        this.logger = logger;
+    }
+
+    [HttpPost]
+    public Task<IActionResult> GetUniqueShareLink([FromBody] LinkShareParams linkShareParams)
+    {
+        throw new NotImplementedException();
+    }
+
+
+    [HttpPost]
+    public Task<IActionResult> GetCourseSelectionFromLink([FromBody] LinkShareParams linkShareParams)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ StudyPlannerAPI/Controllers/Params/LinkShareParams.cs
@@ -0,0 +1,9 @@
+﻿namespace StudyPlannerAPI.Controllers.Params;
+
+public class LinkShareParams
+{
+    public string? UniqueBlob { get; set; }
+    public string? Programme { get; set; }
+    public List<string> MasterCodes { get; set; } = new();
+    public List<string> SelectedCourses { get; set; } = new();
+}
