af824d77825f3c73d8231e2bfd1163897ad4c406
Pontus Salenbo <pontuss98@hotmail.com>
2023-05-03 22:42:23 +0200

    feat: add fetch method. Fix URLS for production and development

--- StudyPlannerUI/src/utils/URL.ts
+++ StudyPlannerUI/src/utils/URL.ts
@@ -1,4 +1,6 @@
-const DEV_URL = 'http://localhost:3000';
-const PROD_URL = 'http://localhost:3000';
+const DEV_URL = 'https://localhost:7266/studyplanner';
+const PROD_URL = 'https://study-planner-api.azurewebsites.net/studyplanner';
 
 export const BASE_URL = process.env.NODE_ENV === 'development' ? DEV_URL : PROD_URL;
+
+export const COURSES_URL = '/courses';
---
+++ StudyPlannerUI/src/utils/fetch.ts
@@ -0,0 +1,12 @@
+import { BASE_URL, COURSES_URL } from './URL';
+
+export async function fetchData(data: unknown) {
+  const body = JSON.stringify(data);
+  const response = await fetch(BASE_URL + COURSES_URL, {
+    body,
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' }
+  });
+  const json = await response.json();
+  return json;
+}
