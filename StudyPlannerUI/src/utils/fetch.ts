b14157bbc087b70a57a978bd2e0ab776bbf088f7
Pontus Salenbo <pontuss98@hotmail.com>
2023-05-09 01:37:35 +0200

    style: perform linting

--- StudyPlannerUI/src/utils/fetch.ts
+++ StudyPlannerUI/src/utils/fetch.ts
@@ -1,12 +1,12 @@
 import { BASE_URL, COURSES_URL } from './URL';
 
 export async function fetchData(data: unknown) {
-  const body = JSON.stringify(data);
-  const response = await fetch(BASE_URL + COURSES_URL, {
-    body,
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' }
-  });
-  const json = await response.json();
-  return json;
+    const body = JSON.stringify(data);
+    const response = await fetch(BASE_URL + COURSES_URL, {
+        body,
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' }
+    });
+    const json = await response.json();
+    return json;
 }
