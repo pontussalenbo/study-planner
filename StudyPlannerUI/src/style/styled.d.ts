3abfbde667e23b268c70afbdc61c0163eec22465
Pontus Salenbo <pontuss98@hotmail.com>
2023-05-12 13:02:00 +0200

    build: change package manager to pnpm. Bump dependencies and minor refactorization

---
+++ StudyPlannerUI/src/style/styled.d.ts
@@ -0,0 +1,8 @@
+// import original module declarations
+import 'styled-components';
+import { Theme } from './theme';
+// and extend them!
+declare module 'styled-components' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface DefaultTheme extends Theme {}
+}
