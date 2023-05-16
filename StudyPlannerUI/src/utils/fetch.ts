52a46b6b3a2ca5ef5b33dc145dd45889c221a949
Pontus Salenbo <pontuss98@hotmail.com>
2023-05-17 00:36:42 +0200

    fix: revamp fetch methods and add error handling

--- StudyPlannerUI/src/utils/fetch.ts
+++ StudyPlannerUI/src/utils/fetch.ts
@@ -1,12 +1,42 @@
-import { BASE_URL, COURSES_URL } from './URL';
+import { BASE_URL } from './URL';
 
-export async function fetchData(data: unknown) {
+export async function POST(endpoint: string, data: unknown) {
     const body = JSON.stringify(data);
-    const response = await fetch(BASE_URL + COURSES_URL, {
-        body,
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' }
-    });
-    const json = await response.json();
-    return json;
+    try {
+        const response = await fetch(BASE_URL + endpoint, {
+            body,
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' }
+        });
+        const json = await response.json();
+        return json;
+    } catch (error) {
+        throw error as APIError;
+    }
+}
+
+export async function GET(endpoint: string, params: URLSearchParams) {
+    try {
+        const response = await fetch(BASE_URL + endpoint + '?' + params.toString());
+        const json = await response.json();
+        return json;
+    } catch (error) {
+        throw error as APIError;
+    }
+}
+
+interface IAPIError {
+  message: string;
+  code: number;
+  rest: Record<string, unknown>;
+}
+class APIError extends Error implements IAPIError {
+    public readonly code;
+    public readonly rest;
+    constructor({ message, code, ...rest }: IAPIError) {
+        super(message);
+        this.name = 'APIError';
+        this.code = code;
+        this.rest = rest;
+    }
 }
