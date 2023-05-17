0cfe881de7e4f46aefc1b83c433eb37098aac209
Pontus Salenbo <pontuss98@hotmail.com>
2023-05-17 18:39:34 +0200

    refactor: add typings and refactor magic strings. Update favicon

---
+++ StudyPlannerUI/src/interfaces/API_Constants.d.ts
@@ -0,0 +1,10 @@
+export const CREDITS_TOTAL_KEY = 'general';
+
+export const Endpoints = {
+    courses: '/courses',
+    classYears: '/general/class_years',
+    academicYears: '/general/academic_years',
+    masters: '/general/masters',
+    programmes: '/general/programmes',
+    masterCheck: '/masters'
+} as const;
--- StudyPlannerUI/src/style/styled.d.ts
+++ StudyPlannerUI/src/style/styled.d.ts
@@ -1,22 +1,8 @@
 // import original module declarations
 import 'styled-components';
+import { Theme } from './theme';
 // and extend them!
 declare module 'styled-components' {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  export interface DefaultTheme {
-    primary: string;
-    secondary: string;
-    accent: string;
-    text: string;
-    button: string;
-    buttonHover: string;
-    header: string;
-    table: string;
-    tableBorder: string;
-    inputBackground: string;
-    inputText: string;
-    boxShadow: string;
-    selectBackground: string;
-    selectText: string;
-  }
+  export interface DefaultTheme extends Theme {}
 }
