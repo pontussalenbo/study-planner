d0bdccda065324978673e01e8faaf0815bc58801
Andreas Bartilson <andreas.bartilson@gmail.com>
2023-05-24 16:11:08 +0200

    Refactor service installation to be open/closed

---
+++ StudyPlannerAPI/Configuration/DatabaseServiceInstaller.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+using System.Data.SQLite;
+using StudyPlannerAPI.Database;
+
+namespace StudyPlannerAPI.Configuration;
+
+public class DatabaseServiceInstaller : IServiceInstaller
+{
+    public void Install(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<IDatabaseQueryManager, DatabaseQueryManager>();
+        services.AddScoped<IDatabaseMutationManager, DatabaseMutationManager>();
+        services.AddScoped<IDbConnection, SQLiteConnection>();
+    }
+}
