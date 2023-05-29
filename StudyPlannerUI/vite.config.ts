516dfbe8c0e111c426adca4954a86b9fe2c4313d
Pontus Salenbo <pontuss98@hotmail.com>
2023-05-29 23:46:28 +0200

    build(deps): bump dependencies. Adjust minor issue in vite

--- StudyPlannerUI/vite.config.ts
+++ StudyPlannerUI/vite.config.ts
@@ -12,82 +12,86 @@ import svgr from 'vite-plugin-svgr';
 import { dependencies } from './package.json';
 
 function renderChunks(deps: Record<string, string>): Record<string, string[]> {
-  const chunks: Record<string, string[]> = {};
-  Object.keys(deps).forEach(key => {
-    if (['react', 'react-dom'].includes(key)) return;
-    chunks[key] = [key];
-  });
-  return chunks;
+    const chunks: Record<string, string[]> = {};
+    Object.keys(deps).forEach(key => {
+        if (['react', 'react-dom'].includes(key)) return;
+        chunks[key] = [key];
+    });
+    return chunks;
 }
 
 export default defineConfig(({ mode }) => ({
-  build: {
-    outDir: 'build',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          ...renderChunks(dependencies)
+    build: {
+        outDir: 'build',
+        rollupOptions: {
+            external: ['fsevents'],
+            output: {
+                manualChunks: {
+                    vendor: ['react', 'react-dom'],
+                    ...renderChunks(dependencies)
+                }
+            }
         }
-      }
-    }
-  },
-  test: {
-    css: false,
-    include: ['src/**/tests/*'],
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: 'src/setupTests.ts',
-    clearMocks: true,
-    coverage: {
-      provider: 'istanbul',
-      enabled: true,
-      '100': true,
-      reporter: ['text', 'lcov'],
-      reportsDirectory: 'coverage'
-    }
-  },
-  plugins: [
-    tsconfigPaths(),
-    svgr(),
-    react(),
-    ...(mode === 'test'
-      ? []
-      : [
-        eslintPlugin(),
-        VitePWA({
-          registerType: 'autoUpdate',
-          includeAssets: [
-            'favicon.png',
-            'robots.txt',
-            'apple-touch-icon.png',
-            'icons/*.svg',
-            'fonts/*.woff2'
-          ],
-          manifest: {
-            theme_color: '#BD34FE',
-            icons: [
-              {
-                src: '/android-chrome-192x192.png',
-                sizes: '192x192',
-                type: 'image/png',
-                purpose: 'any maskable'
-              },
-              {
-                src: '/android-chrome-512x512.png',
-                sizes: '512x512',
-                type: 'image/png'
-              }
-            ]
-          }
-        })
-      ]),
-    visualizer({
+    },
+    test: {
+        css: false,
+        include: ['src/**/tests/*'],
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: 'src/setupTests.ts',
+        clearMocks: true,
+        coverage: {
+            provider: 'istanbul',
+            enabled: true,
+            '100': true,
+            reporter: ['text', 'lcov'],
+            reportsDirectory: 'coverage'
+        }
+    },
+    plugins: [
+        tsconfigPaths(),
+        svgr(),
+        react(),
+        ...(mode === 'test'
+            ? []
+            : [
+                eslintPlugin(),
+                VitePWA({
+                    registerType: 'autoUpdate',
+                    includeAssets: [
+                        'favicon.png',
+                        'robots.txt',
+                        'apple-touch-icon.png',
+                        'icons/*.svg',
+                        'fonts/*.woff2'
+                    ],
+                    manifest: {
+                        theme_color: '#BD34FE',
+                        icons: [
+                            {
+                                src: '/android-chrome-192x192.png',
+                                sizes: '192x192',
+                                type: 'image/png',
+                                purpose: 'any maskable'
+                            },
+                            {
+                                src: '/android-chrome-512x512.png',
+                                sizes: '512x512',
+                                type: 'image/png'
+                            }
+                        ]
+                    }
+                })
+            ])
+    ]
+}));
+
+/**
+ *     visualizer({
       template: 'treemap', // or sunburst
       open: true,
       gzipSize: true,
       brotliSize: true,
       filename: 'build/analyze.html'
     })
-  ]
-}));
+ */
