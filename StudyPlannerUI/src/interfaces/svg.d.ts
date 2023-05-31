fa4056bee01d3b00156e6e935ec8f766251bd131
Pontus Salenbo <pontuss98@hotmail.com>
2023-06-01 01:01:05 +0200

    chore: add README draft. Update linter configs

--- StudyPlannerUI/src/interfaces/svg.d.ts
+++ StudyPlannerUI/src/interfaces/svg.d.ts
@@ -1,13 +1,11 @@
 declare module '*.svg' {
-  import * as React from 'react';
+    import * as React from 'react';
 
-  export const ReactComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement> & { title?: string }
-  >;
+    export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement> & { title?: string }>;
 
-  const content: {
-    MyIcon: React.FunctionComponent<React.SVGProps<SVGSVGElement> & { title?: string }>;
-  };
+    const content: {
+        MyIcon: React.FunctionComponent<React.SVGProps<SVGSVGElement> & { title?: string }>;
+    };
 
-  export default content;
+    export default content;
 }
