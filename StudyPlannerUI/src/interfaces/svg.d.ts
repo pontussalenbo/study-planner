31bfb286da524b2b5bf8a83f0d0402d3ba6f7e83
Pontus Salenbo <pontuss98@hotmail.com>
2023-05-31 16:03:55 +0200

    Add support for SVG as Components. Will remove errors regarding missing files. Solves #36

---
+++ StudyPlannerUI/src/interfaces/svg.d.ts
@@ -0,0 +1,13 @@
+declare module '*.svg' {
+  import * as React from 'react';
+
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+
+  const content: {
+    MyIcon: React.FunctionComponent<React.SVGProps<SVGSVGElement> & { title?: string }>;
+  };
+
+  export default content;
+}
