96b26da8bb2c075361852b17e56e384e79c41f61
Pontus Salenbo <pontuss98@hotmail.com>
2023-06-01 10:25:36 +0200

    fix: minor CSS styling adjustments

--- StudyPlannerUI/src/utils/breakpoints.ts
+++ StudyPlannerUI/src/utils/breakpoints.ts
@@ -1,20 +1,25 @@
 export const breakpoints = {
+    /** xs-breakpoint (0px) */
     xs: 0, // Extra small devices (portrait phones)
+    /** sm-breakpoint (576px) */
     sm: 576, // Small devices (landscape phones)
+    /** md-breakpoint (768px) */
     md: 768, // Medium devices (tablets)
+    /** lg-breakpoint (992px) */
     lg: 992, // Large devices (desktops)
+    /** xl-breakpoint (1200px) */
     xl: 1200 // Extra large devices (large desktops)
 };
 
 export const device = {
-    /** xs-breakpoint */
+    /** xs-breakpoint (0px) */
     xs: `(min-width: ${breakpoints.xs}px)`,
-    /** sm-breakpoint */
+    /** sm-breakpoint (576px) */
     sm: `(min-width: ${breakpoints.sm}px)`,
-    /** md-breakpoint */
+    /** md-breakpoint (768px) */
     md: `(min-width: ${breakpoints.md}px)`,
-    /** lg-breakpoint */
+    /** lg-breakpoint (992px) */
     lg: `(min-width: ${breakpoints.lg}px)`,
-    /** xl-breakpoint */
+    /** xl-breakpoint (1200px) */
     xl: `(min-width: ${breakpoints.xl}px)`
 };
