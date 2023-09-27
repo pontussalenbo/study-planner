ac4446d2ce03cc19773ccd43a16d7c8c96daef6c
Pontus Salenbo <pontuss98@hotmail.com>
2023-09-27 22:25:14 +0200

    refactor: remove unused code. Fix typos and errors in formatting

--- StudyPlannerUI/src/utils/getUniqueList.ts
+++
@@ -1,6 +0,0 @@
-// TODO: Remove?
-export function getUniqueListBy<T>(arr: T[], key: keyof T): T[] {
-    return [...new Map(arr.map(item => [item[key], item])).values()];
-}
-
-export default getUniqueListBy;
